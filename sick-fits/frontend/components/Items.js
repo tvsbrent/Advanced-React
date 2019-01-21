aa902f9013df481a2d3970da60ca389c25bcd4a3
Brent Schmidt <bschmidt@oreilly.com>
2019-01-21 10:55:35 -0500

    Add Pagination

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -3,10 +3,12 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
+import Pagination from './Pagination';
+import { perPage } from '../config';
 
 export const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -32,18 +34,27 @@ const ItemsList = styled.div`
 class Items extends Component {
   render() {
     return (
-      <div>
-        <Query query={ALL_ITEMS_QUERY}>{
-          ({ data, error, loading }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error: {error.message}</p>;
+      <Center>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: (this.props.page * perPage) - perPage,
+            first: perPage
+          }}>{
+            ({ data, error, loading }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error: {error.message}</p>;
 
-            return (<ItemsList>
-              {data.items.map(item => <Item key={item.id} item={item} />)}
-            </ItemsList>);
-          }
-        }</Query>
-      </div>
+              return (
+                <ItemsList>
+                  {data.items.map(item => <Item key={item.id} item={item} />)}
+                </ItemsList>
+              );
+            }
+          }</Query>
+        <Pagination page={this.props.page} />
+      </Center>
     );
   }
 }
